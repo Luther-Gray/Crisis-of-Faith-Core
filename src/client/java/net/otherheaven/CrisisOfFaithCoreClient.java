2035e9b49e4b42dc9637f3ef46b50bc60698ed89
Luther Gray <luther_the_developer@proton.me>
2025-10-02 21:19:03 -0400

    + Porting from Codeberg

---
+++ src/client/java/net/otherheaven/CrisisOfFaithCoreClient.java
@@ -0,0 +1,10 @@
+package net.otherheaven;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrisisOfFaithCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
